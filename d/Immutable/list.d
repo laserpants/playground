dbb2959146787b2984aae9ae1408d8f94304130b
laserpants <johannes@isomorphic.co>
2016-04-06 13:26:29 +0300

    create module and package namespace

---
+++ d/Immutable/list.d
@@ -0,0 +1,69 @@
+module Immutable.list;
+
+import std.typecons;
+
+immutable class List(T)
+{
+    alias Cons = Tuple!(immutable T, "head", immutable List, "tail");
+
+    static immutable class Item
+    {
+        this(immutable T data, immutable Item next) immutable 
+        {
+            this.data = data;
+            this.next = next;
+        }
+        
+        immutable T data;
+        immutable Item next;
+    }
+
+    this() immutable 
+    {
+        _head = null;
+        length = 0;
+    }
+
+    this(immutable T data, immutable List tail) immutable 
+    {
+        _head = new immutable Item(data, tail._head);
+        length = tail.length + 1;
+    }
+
+    private this(immutable Item head, ulong length) immutable
+    {
+        _head = head;
+        this.length = length;
+    }
+
+    static immutable(List) fromArray(immutable T[] arr) pure
+    {
+        immutable(List) delegate(immutable List, immutable long) pure f;
+        f = delegate (list, index) => index == -1 
+            ? list 
+            : f(new immutable List(arr[index], list), index - 1);
+    
+        return f(List.nil(), arr.length - 1);
+    }
+
+    static immutable(List) cons(immutable T head, immutable List tail) pure { return new immutable List(head, tail); }
+    static immutable(List) nil() pure { return new immutable List; }
+
+    bool empty() pure { return _head is null; }
+
+    immutable(T) head() pure { assert(!empty()); return _head.data; }
+    immutable(List) tail() pure { assert(!empty()); return new immutable List(_head.next, length - 1); }
+
+    immutable Cons destruct() pure { return Cons(_head.data, tail()); }
+
+    immutable(List) take(ulong n)
+    {
+        return 0 == n 
+            ? new immutable List
+            : new immutable List(_head.data, tail().take(n - 1));
+    }
+
+    public immutable ulong length;
+
+    private immutable Item _head;
+}
