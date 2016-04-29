71674547af401728398283ef1605d52f6fe21150
Johannes Hilden <hildenjohannes@gmail.com>
2016-04-29 08:37:09 +0300

    clean up tree

--- elm/Hello.elm
+++ elm/Hello.elm
@@ -2,12 +2,12 @@ import Color            exposing (..)
 import Graphics.Element exposing (..)
 import Graphics.Collage exposing (..)
 import Transform2D      exposing (..)
-import Text             exposing ( fromString )
+import Text             exposing (..)
 
 label : a -> Float -> Form
 label elm size = 
   elm |> toString
-      |> fromString
+      |> Text.fromString
       |> Text.height size
       |> text
 
@@ -22,47 +22,54 @@ pathTo = segment (0, 0)
 
 type Direction = Left | Right
 
-leg : Direction -> (Float, Float) -> Path
-leg direction (x, y) = 
-  case direction of
-    Left  -> pathTo ( x, y)
-    Right -> pathTo (-x, y)
+leg : (Float, Float) -> Maybe Direction -> List Path
+leg point direction = 
+  let
+    path (x, y) dir = 
+      case dir of
+        Left  -> pathTo ( x, y)
+        Right -> pathTo (-x, y) 
+  in 
+    Maybe.map2 path (Just point) direction |> maybeToList
 
-legs : (Float, Float) -> Maybe Direction -> List Path
-legs point direction = 
-  case Maybe.map2 leg direction (Just point) of
-    Nothing -> [ ]
-    Just a  -> [a]
+wedgeWidth : Float -> Float -> Float
+wedgeWidth w h = w / (2^h + 2^(h - 1) - 1) * (3 / 8) * 2^h
 
-wedgeWidth : Float -> Height -> Float
-wedgeWidth w h = w / toFloat (2 ^ h + 2 ^ (h - 1) - 1) * (3 / 8) * 2 ^ toFloat h
-
-treeForm : Float -> Tree a -> Form
-treeForm nodeSize tree = 
+tree  : Int 
+     -> Int 
+     -> Float 
+     -> Tree a 
+     -> Element
+tree xSize ySize nodeSize tree = 
   let 
-    h = height tree
-    yOffs = 400 / (toFloat h - 1)
-    legForm : Path -> Form
-    legForm = moveY (yOffs / 2) << traced defaultLine
+    xSize'     = toFloat xSize - nodeSize * 2 - 2
+    ySize'     = toFloat ySize - nodeSize * 2 - 2
+    treeHeight = toFloat (height tree)
+    yOffs      = ySize' / (treeHeight - 1)
+    legForm    = moveY (yOffs / 2) << traced defaultLine
     subtree : Maybe Direction -> Tree a -> Float -> List Form
     subtree direction tree xOffs = 
       case tree of 
         Empty -> []
         Node left elm right -> 
-          List.map legForm (legs (2 * xOffs, yOffs) direction)
+          List.map legForm (leg (2 * xOffs, yOffs) direction)
        ++ List.map (move (-xOffs, -yOffs)) (subtree (Just Left ) left  (xOffs / 2))
        ++ List.map (move ( xOffs, -yOffs)) (subtree (Just Right) right (xOffs / 2))
        ++ [ node elm nodeSize |> moveY (yOffs / 2) ]
-  in group 
-      [ moveY (200 - yOffs / 2) (group <| subtree Nothing tree <| wedgeWidth 400 h)
-      , outlined defaultLine (rect 400 400) 
-      ]
+  in collage xSize ySize [ group 
+      [ (group <| subtree Nothing tree 
+               <| wedgeWidth xSize' treeHeight) 
+                |> moveY ((ySize' - yOffs) / 2) ] ]
+
+maybeToList : Maybe a -> List a
+maybeToList val =
+  case val of
+    Nothing -> [ ]
+    Just a  -> [a]
 
 type Tree a = Empty | Node (Tree a) a (Tree a)
 
-type alias Height = Int
-
-height : Tree a -> Height
+height : Tree a -> Int
 height tree = 
   case tree of
     Empty -> 0 
@@ -73,4 +80,4 @@ height tree =
 myTree = Node (Node (Node (Node Empty 1 Empty) 2 (Node Empty 3 Empty)) 4 (Node Empty 5 Empty)) 6 (Node Empty 7 Empty) --Node (Node (Node (Node (Node (Node Empty 0 Empty) 0 (Node Empty 0 Empty)) 1 (Node Empty 2 Empty)) 2 (Node Empty 3 Empty)) 4 Empty) 5 (Node Empty 7 Empty)
 
 main : Element
-main = collage 500 700 [ treeForm 18 myTree ]
+main = tree 500 300 15 myTree 
